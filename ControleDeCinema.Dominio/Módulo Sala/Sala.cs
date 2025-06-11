51e863ed646e3465907d27bde65819de699184db
Leonardo Rodrigues <Lthkrieger@gmail.com>
2025-06-11 16:15:33 -0300

    Retirado função Desocupar(), implementação com falha lógica;

--- ControleDeCinema.Dominio/Módulo Sala/Sala.cs
+++ ControleDeCinema.Dominio/Módulo Sala/Sala.cs
@@ -19,16 +19,6 @@ public class Sala : EntidadeBase
     public string Nome { get; set; }
     public bool Disponivel { get; set; }
 
-    public void Desocupar()
-    {
-        DateTime horaDeFinalizacao = DateTime.Now;
-
-        DateTime tempoDeLimpeza = horaDeFinalizacao.AddMinutes(20);
-
-        if (tempoDeLimpeza == DateTime.Now)
-            Disponivel = true;
-    }
-
     public void Ocupar()
     {
         Disponivel = false;
